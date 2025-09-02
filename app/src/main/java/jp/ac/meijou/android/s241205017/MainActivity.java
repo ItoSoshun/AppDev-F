8e8cc6aa5e5777fecad85013115478b3d033f426
伊藤創俊 <241205017@ccmailg.meijo-u.ac.jp>
2025-09-02 12:08:31 +0900

    演習-テキストを編集する

--- app/src/main/java/jp/ac/meijou/android/s241205017/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205017/MainActivity.java
@@ -7,18 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import jp.ac.meijou.android.s241205017.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.id017.setText(R.string.text2);
     }
+
+
 }
