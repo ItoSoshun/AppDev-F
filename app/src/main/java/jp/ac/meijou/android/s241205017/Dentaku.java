ddf6d596e3e7355bdc1c726a458976e974055317
伊藤創俊 <241205017@ccmailg.meijo-u.ac.jp>
2025-09-03 14:34:53 +0900

    演習-Day3 画面遷移を作った

--- app/src/main/java/jp/ac/meijou/android/s241205017/Dentaku.java
+++ app/src/main/java/jp/ac/meijou/android/s241205017/Dentaku.java
@@ -8,13 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205017.databinding.ActivityMainBinding;
+
 public class Dentaku extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dentaku);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,3 +26,60 @@ public class Dentaku extends AppCompatActivity {
         });
     }
 }
+
+
+/*
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.button.setOnClickListener(v -> {
+            var text2 = binding.editTextText.getText().toString();
+            binding.id017.setText(text2);
+        });
+
+
+
+        prefDataStore = PrefDataStore.getInstance(this);
+
+        //prefDataStore.getString("name").ifPresent(name -> binding.id017.setText(name));
+
+        binding.button.setOnClickListener(v -> {
+            var text2 = binding.editTextText.getText().toString();
+            binding.id017.setText(text2);
+        });
+
+        binding.saveButton.setOnClickListener(v -> {
+            var text2 = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text2);
+        });
+    }
+
+    protected void onStart() {
+        super.onStart();
+        prefDataStore.getString("name").ifPresent(name -> binding.id017.setText(name));
+    }
+
+
+
+
+
+
+}
+*/
